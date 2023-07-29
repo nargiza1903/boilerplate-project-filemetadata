e7072dcf53da531b863b921dc6360f99a24dd2c9
nargiza1903 <121645300+nargiza1903@users.noreply.github.com>
2023-07-30 01:35:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+// Import the 'express' module as express
 var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
 
+// Import the 'cors' (Cross-Origin Resource Sharing) module as cors. This is used to enable cross origins requests
+var cors = require('cors');
+
+// Use 'dotenv' module to load environment variables from a .env file into process.env
+require('dotenv').config();
+
+// Create an Express application instance
 var app = express();
 
+// Use CORS middleware on the app. This sets up some headers on the HTTP responses telling browsers to allow us to make requests from our client-side JavaScript code to our server
 app.use(cors());
+
+// Serve static files (HTML, CSS, JavaScript, images, etc.) from the 'public' directory
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Define a root ('/') route handler for GET requests. This sends the 'index.html' file when someone navigates to the root of our website
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set the port based on an environment variable, or if there's nothing set, default to 3000. This is the port at which our server will be running
+const serverPort = process.env.PORT || 3000;
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+// Have the Express application listen on the given port. Once the server is running, it logs a message to the console
+app.listen(serverPort, function () {
+  console.log('Your server is up and running on port ' + serverPort)
 });
